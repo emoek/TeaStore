87d31e30c77932dfab1f5c9e120885f395102ccc
oeksuez <oeksuezemre@outlook.de>
2023-12-14 18:11:15 +0100

    adjusted symlink script

--- examples/kubernetes/teastore-rabbitmq-promtail/entrypoint.sh
+++ examples/kubernetes/teastore-rabbitmq-promtail/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "USING ENTRYPOINT SCRIPT TO CREATE SYMLINK!"
+mkdir symlogs
 while true; do
     log_file=$(find /apache-tomcat-8.5.24/webapps/logs/kieker-* -type f -name 'kieker-*' -print -quit 2>/dev/null)
     if [ -n "$log_file" ]; then
 	echo "SYMLINK CREATED!"    
-        ln -s "$log_file" sym-kieker-output.dat
+        ln -s "$log_file" /symlogs/sym-kieker-output.dat
         break
     fi
     sleep 1
