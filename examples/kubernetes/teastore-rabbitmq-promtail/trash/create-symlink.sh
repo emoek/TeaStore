3a5e61b07d33efef050e6304df0d63d167da8435
oeksuez <oeksuezemre@outlook.de>
2024-04-20 21:10:26 +0200

    cleanup

--- examples/kubernetes/teastore-rabbitmq-promtail/trash/create-symlink.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "CREATING SYMLINK FOR KIEKER OUTPUT!"
-ln -s /apache-tomcat-8.5.24/webapps/logs/kieker-*/kieker-* output.dat
--- examples/theodolite/trash/run_request.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# The script sends an HTTP GET request
-curl http://teastore-webui:8080/tools.descartes.teastore.webui/
-
